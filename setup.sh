20ba4b559bcaa718a3f86c74a9296e248b2b45da
demo-user <demo-user@localhost>
2024-01-22 13:17:25 -0500

    output to dev null

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # install AWS CLI
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" > /dev/null
+unzip awscliv2.zip > /dev/null
 sudo ./aws/install
 
 # Create a Namespace
